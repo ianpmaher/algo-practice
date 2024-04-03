d25e6e042261349392c6c2bcc75825487a7211d3
Ian <ianpmaher@gmail.com>
2024-04-03 18:25:02 -0400

    python containsDuplicate

---
+++ leetcode/containsDuplicates.py
@@ -0,0 +1,21 @@
+# Given an array nums of integers, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+# Input: nums = [1,2,3,1]
+# Output: true
+
+# Example 2:
+# Input: nums = [1,2,3,4]
+# Output: false
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num) #add to hashset if not already in hashset
+        return False
+    
+# Time complexity: O(n)
+# Space complexity: O(n)
