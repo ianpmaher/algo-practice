4ccb4aaaf3e1be7f9ee9a156881a5b360c96eaad
Ian Maher <ianpmaher@gmail.com>
2024-10-20 21:24:04 -0400

    quick lil algo

---
+++ leetcode/palindromeChecker.js
@@ -0,0 +1,23 @@
+// 125. valid palindrome 
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// Example 1:
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true // Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+// Input: s = "race a car"
+// Output: false // Explanation: "raceacar" is not a palindrome.
+function isPalindrome(s) {
+    // regex to remove non letter characters
+    var lowercaseNormalized = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // string to array, reverse method, then join ==> .join("") = new string
+    var reversed = lowercaseNormalized.split("").reverse().join("");
+    if (lowercaseNormalized == reversed) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+;
+console.log(isPalindrome("racecar")); // true
